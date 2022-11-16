d119f294e96d9b0c1f3703c22bd9ae562ed1bf1e
Rushikesh Badgujar <82447941+rishi7822@users.noreply.github.com>
2022-11-17 01:01:21 +0530

    Add files via upload

---
+++ removelist.py
@@ -0,0 +1,18 @@
+#remove function
+
+companies = ["google","amazon","tesla","microsoft","cognizant"]
+companies.remove("google")
+print(companies)
+
+#pop using index nnumbers
+companies.pop(1)
+print(companies)
+
+#delete functions
+del companies
+
+
+#clear method
+companies.clear()
+print(companies)
+
